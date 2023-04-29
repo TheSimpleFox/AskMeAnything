ab3ab95af266690e3c94f02666160c462cea8e48
Matt Halliday <matthalliday@hey.com>
2023-04-29 19:06:07 -0400

    Configure React and esbuild

---
+++ app/javascript/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <div>Hello, AskMeAnything!</div>
+    </React.StrictMode>,
+  document.getElementById("root"));
+});
