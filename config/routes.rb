bcb6fa3c93622df2913b87cf25786d40ed077184
Matt Halliday <matthalliday@hey.com>
2023-04-29 23:59:55 -0400

    Define catch all route in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "entry#index"
+
+  # Define a catch all route so that React Router works
+  get '*path', to: 'entry#index'
 end
